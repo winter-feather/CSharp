291383af38d8cbc9eb823a46026606efd0fb99e0
lisuo01 <lisuo01@58.com>
2022-05-05 15:58:28 +0800

    package测试

---
+++ Runtime/CustomePackage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomePackage : MonoBehaviour
+{
+    public int testInt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
